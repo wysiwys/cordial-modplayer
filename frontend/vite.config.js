b67970fbdd61332b47ed9bc9eaee38b7e0382f17
wysiwys <144733119+wysiwys@users.noreply.github.com>
2024-08-28 13:33:41 -0400

    add flag for adding base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 import license from 'rollup-plugin-license';
 import vue from '@vitejs/plugin-vue';
 
+function basePath() {
+    try {
+        return process.env.BASE_PATH;
+    } catch (e) {
+        return '';
+    }
+}
+
 function hasBackend() {
     try {
         let i = parseInt(process.env.HAS_BACKEND);
@@ -12,8 +20,7 @@ function hasBackend() {
     }
 }
 export default defineConfig({
-    // TODO: fix for desktop player
-    base: '/cordial-modplayer',
+    base: basePath(),
     define: {
         __HAS_BACKEND__: hasBackend(),
     },
